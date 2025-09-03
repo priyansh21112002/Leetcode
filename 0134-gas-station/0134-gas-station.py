67e70db858973c26066088eca535d63f7b6a98ed
Priyansh Srivastava <82864517+priyansh21112002@users.noreply.github.com>
2025-09-03 23:08:47 +0530

    Time: 20 ms (63.45%), Space: 23 MB (99.65%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,13 +1,20 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], consumption: List[int]) -> int:
-        total_tank, curr_tank, start = 0, 0, 0
+        n = len(gas)
 
-        for i in range(len(gas)):
-            total_tank += gas[i] - consumption[i]
+        # Global feasibility check
+        if sum(gas) < sum(consumption):
+            return -1
+
+        curr_tank = 0
+        start = 0
+
+        for i in range(n):
             curr_tank += gas[i] - consumption[i]
 
+            # If fuel drops below 0, reset start
             if curr_tank < 0:
                 start = i + 1
                 curr_tank = 0
 
-        return start if total_tank >= 0 else -1
+        return start
